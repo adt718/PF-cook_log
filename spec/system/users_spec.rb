6393a926876889d6235d295a1d39df1bdc649187
adt718 <ksgses531@gmail.com>
2021-02-09 14:21:10 +0000

    add gravatar

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,19 +2,61 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
   let!(:user) { create(:user) }
+  let!(:admin_user) { create(:user, :admin) }
 
+  describe "ユーザー一覧ページ" do
+    context "管理者ユーザーの場合" do
+      it "ぺージネーション、自分以外のユーザーの削除ボタンが表示されること" do
+        create_list(:user, 30)
+        login_for_system(admin_user)
+        visit users_path
+        expect(page).to have_css "div.pagination"
+        User.paginate(page: 1).each do |u|
+          expect(page).to have_link u.name, href: user_path(u)
+          expect(page).to have_content "#{u.name} | 削除" unless u == admin_user
+        end
+      end
+    end
+
+     context "管理者ユーザー以外の場合" do
+      it "ぺージネーション、自分のアカウントのみ削除ボタンが表示されること" do
+        create_list(:user, 30)
+        login_for_system(user)
+        visit users_path
+        expect(page).to have_css "div.pagination"
+        User.paginate(page: 1).each do |u|
+          expect(page).to have_link u.name, href: user_path(u)
+          if u == user
+            expect(page).to have_content "#{u.name} | 削除"
+          else
+            expect(page).not_to have_content "#{u.name} | 削除"
+          end
+        end
+      end
+    end
+  end
 
   describe "ユーザー登録ページ" do
     before do
       visit signup_path
     end
 
-describe "プロフィール編集ページ" do
+  describe "プロフィール編集ページ" do
     before do
       login_for_system(user)
       visit user_path(user)
       click_link "プロフィール編集"
     end
+
+    context "アカウント削除処理", js: true do
+      it "正しく削除できること" do
+        click_link "アカウントを削除する"
+        page.driver.browser.switch_to.alert.accept
+        expect(page).to have_content "自分のアカウントを削除しました"
+      end
+    end
+  end
+
 
     context "ページレイアウト" do
       it "正しいタイトルが表示されることを確認" do
@@ -106,4 +148,3 @@ describe "プロフィール編集ページ" do
       end
     end
   end
-end
