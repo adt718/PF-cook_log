f68cf2e8313d3e3daa560e03e77c2487eeef3e2e
adt718 <ksgses531@gmail.com>
2021-03-21 11:30:07 +0000

    fix list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,9 +6,10 @@ class ListsController < ApplicationController
   def index
     @lists = current_user.lists.paginate(page: params[:page], per_page: 5)
     @log = Log.new
-    dish_ids = []
-    @lists.each do |list|
-      dish_ids.append(list.dish_id)
+    # dish_ids = []
+    # @lists.each do |list|
+    #   dish_ids.append(list.dish_id)
+    dish_ids = @lists.map do |list| list.dish_id
     end
     @dishes = Dish.where(id: dish_ids)
   end
