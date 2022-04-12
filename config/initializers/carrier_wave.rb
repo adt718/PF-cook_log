a7c6043af1485212b3301703fb4c47c2ff0aa83e
adt718 <ksgses531@gmail.com>
2022-04-12 06:40:53 +0000

    fix fog

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,24 +1,24 @@
-# # CarrierWaveの設定呼び出し
+# CarrierWaveの設定呼び出し
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
-# # 画像名に日本語が使えるようにする
-# CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
-# CarrierWave.configure do |config|
-#   # 本番環境はS3に保存
-#   if Rails.env.production?
-#     config.storage = :fog
-#     config.fog_provider = 'fog/aws'
-#     config.fog_directory = 'pf-cooklog'
-#     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/pf-cooklog'
-#     config.fog_public = false
-#     config.fog_credentials = {
-#       provider: 'AWS',
-#       # credentialsで管理する場合
-#       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-#       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-#       region: 'ap-northeast-1'
-#     }
-#   else
-#     config.storage :file
-#   end
-# end
+# 画像名に日本語が使えるようにする
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+CarrierWave.configure do |config|
+  # 本番環境はS3に保存
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory = 'cooklogs'
+    config.asset_host = 'https://s3-ap-northeast-3.amazonaws.com/cooklogs'
+    config.fog_public = false
+    config.fog_credentials = {
+      provider: 'AWS',
+      # credentialsで管理する場合
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-3'
+    }
+  else
+    config.storage :file
+  end
+end
