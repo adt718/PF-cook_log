c414937c9b9a4948374f89690147c9c7659f60a4
adt718 <ksgses531@gmail.com>
2022-04-13 00:14:18 +0000

    fix controller

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -61,7 +61,7 @@ class DishesController < ApplicationController
       flash[:success] = '料理が登録されました！'
       Log.create(dish_id: @dish.id, content: @dish.cook_memo)
       # current_user.list(@dish))
-      redirect_to dish_path(@dish)
+      redirect_to dish_path(@dish.id)
     else
       render 'dishes/new'
     end
