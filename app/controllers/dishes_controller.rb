2df0ddb4d5e4bba06f9c660a6c00078acdc74679
adt718 <ksgses531@gmail.com>
2022-04-15 07:49:13 +0000

    change production

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -58,7 +58,6 @@ class DishesController < ApplicationController
   def create
     # binding.pry
     @dish = current_user.dishes.build(dish_params)
-    Dish.create(dish_params)
     if @dish.save
       flash[:success] = '料理が登録されました！'
       Log.create(dish_id: @dish.id, content: @dish.cook_memo)
